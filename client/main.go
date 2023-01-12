742c02a0787eedfdf4349030688cbc1ffd74536c
germansanz93 <germansanz93@gmail.com>
2023-01-11 22:28:39 -0300

    implementando clientes serverStreaming y BidirectionalStreaming

--- client/main.go
+++ client/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"germansanz93/go/grpc/testpb"
+	"io"
 	"log"
 	"time"
 
@@ -22,7 +23,9 @@ func main() {
 	c := testpb.NewTestServiceClient(cc)
 
 	// DoUnary(c)
-	DoClientStreaming(c)
+	// DoClientStreaming(c)
+	// DoServerStreaming(c)
+	DoBiDirectionStreaming(c)
 }
 
 func DoUnary(c testpb.TestServiceClient) {
@@ -79,4 +82,65 @@ func DoClientStreaming(c testpb.TestServiceClient) {
 	}
 	log.Printf("response from server: %v", msg)
 
+}
+
+func DoServerStreaming(c testpb.TestServiceClient) {
+	req := &testpb.GetStudentsPerTestRequest{
+		TestId: "t1",
+	}
+
+	stream, err := c.GetStudentsPerTest(context.Background(), req)
+
+	if err != nil {
+		log.Fatalf("error while calling GetStudentsPerTest: %v", err)
+	}
+
+	for {
+		msg, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("error while reading stream: %v", err)
+		}
+		log.Printf("response from server: %v", msg)
+	}
+}
+
+func DoBiDirectionStreaming(c testpb.TestServiceClient) {
+	answer := testpb.TakeTestRequest{
+		Answer: "42",
+	}
+
+	numberOfQuestions := 4
+
+	waitChannel := make(chan struct{})
+
+	stream, err := c.TakeTest(context.Background())
+	if err != nil {
+		log.Fatalf("error while calling TakeTest: %v", err)
+	}
+
+	go func() {
+		for i := 0; i < numberOfQuestions; i++ {
+			stream.Send(&answer)
+			time.Sleep(2 * time.Second)
+		}
+	}()
+
+	go func() {
+		for {
+			res, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalf("error while reading stream: %v", err)
+				break
+			}
+			log.Printf("response from server: %v", res)
+		}
+		close(waitChannel)
+	}()
+	<-waitChannel
 }
